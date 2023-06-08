7e93160b45383df85b3e93e860f9c01bc3020f0b
ChrisMitchell99 <133904346+ChrisMitchell99@users.noreply.github.com>
2023-06-08 20:48:54 +0100

    Add files via upload

---
+++ ex11_starter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+print(Belgium)
+print(len(Belgium))
+
+# hyphens = Belgium.replace('','-')
+#
+# print(hyphens)
+
+print('-' * 81)
+
+colons = Belgium.replace(',', ':')
+print(colons)
+
+
+
+
+
